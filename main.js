9c4f175e348133b16e116aacd05bb60947e48368
Luu Van <vanngocluu94@gmail.com>
2020-10-01 23:08:51 +0700

    add cart

--- main.js
+++ main.js
@@ -36,6 +36,14 @@ for (let i=0; i < carts.length; i++) {
     })
 }
 
+function onLoadCartNumbers(){
+    let productNumbers = localStorage.getItem('cartNumbers');
+
+    if(productNumbers){
+        document.querySelector('.cart span').textContent = productNumbers;
+    }
+}
+
 function cartNumbers() {
     let productNumbers = localStorage.getItem('cartNumbers');
 
@@ -43,8 +51,12 @@ function cartNumbers() {
 
     if(productNumbers) {
         localStorage.setItem('cartNumbers', productNumbers + 1);
+        document.querySelector('.cart span').textContent = productNumbers + 1;
     } else {
         localStorage.setItem('cartNumbers', 1);
+        document.querySelector('.cart span').textContent = 1;
     }
     
 }
+
+onLoadCartNumbers();
